e12f422e7ade8dd9e138419219657c17de2012a4
Autumn Tandog <k1tsunebby0225@gmail.com>
2026-06-05 02:02:12 +0000

    style: moved the DeviceInfo under TodayHighlight

--- src/App.jsx
+++ src/App.jsx
@@ -86,12 +86,12 @@ function App() {
                   <div className="flex flex-col gap-6 mx-auto max-w-6xl">
                     <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                       <div className="lg:col-span-7 flex flex-col gap-6">
-                        <DeviceInfo cityName={selectedCity?.name} countryCode={selectedCity?.country} />
                         <CurrentWeather data={weatherData.current} unit={unit} />
                         <ForecastCard data={weatherData.daily} unit={unit} onOpenOutlook={() => setShowOutlook(true)} />
                       </div>
-                      <div className="lg:col-span-5">
+                      <div className="lg:col-span-5 flex flex-col gap-6">
                         <TodayHighlight data={weatherData.current} hourly={weatherData.hourly} />
+                        <DeviceInfo cityName={selectedCity?.name} countryCode={selectedCity?.country} />
                       </div>
                     </div>
                   </div>
